5ffdd470a060c50f8fffa9bb258258009296a35e
vineetc17 <87166771+vineetc17@users.noreply.github.com>
2022-06-15 19:40:24 +0530

    Update PassengerCounter.js

--- PassengerCounter.js
+++ PassengerCounter.js
@@ -1,32 +1,3 @@
-// let myAge = 19;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
-
-// let count = 4;
-// count = count + 1;
-// console.log(count);
-
-// let bonusPoints = 50
-
-// for(let i=1;i<=50;i++)
-// {
-//     bonusPoints = bonusPoints + 1;
-// }
-// console.log(bonusPoints);
-
-// for(let i=1;i<=75;i++)
-// {
-//     bonusPoints = bonusPoints - 1;
-// }
-// console.log(bonusPoints);
-
-// for(let i=1;i<=45;i++)
-// {
-//     bonusPoints = bonusPoints + 1;
-// }
-// console.log(bonusPoints);
-
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-element");
 console.log(saveEl);
